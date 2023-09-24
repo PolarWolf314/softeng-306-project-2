867930b1bab8394de83f7f983509e1693fafbd92
pumbas600 <joshjeffers600@gmail.com>
2023-09-24 13:45:35 +1300

    feat: :sparkles: Create argument parser for extracting required arguments

--- src/main/java/se306/group12/Main.java
+++ src/main/java/se306/group12/Main.java
@@ -1,13 +1,47 @@
 package se306.group12;
 
-import se306.group12.models.Test;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
 public class Main {
 
     public static void main(final String[] args) {
-        System.out.println("Hello world!");
+        final ArgumentParser parser = ArgumentParsers.newFor("scheduler.jar").build()
+            .description(
+                "An algorithm for finding the optimal schedule for a given set of tasks and processors.");
+        parser.addArgument("inputDotGraph")
+            .metavar("INPUT.dot")
+            .required(true)
+            .help("A task graph with integer weights in the dot format");
+        parser.addArgument("processes")
+            .metavar("P")
+            .required(true)
+            .type(Integer.class)
+            .help("The number of processes to schedule the INPUT graph on");
+        parser.addArgument("-p", "--parallel")
+            .metavar("N")
+            .type(Integer.class)
+            .dest("parallelProcesses")
+            .setDefault(1)
+            .help("Use N cores for execution in parallel (default is sequential)");
+        parser.addArgument("-v", "--visualise")
+            .action(Arguments.storeTrue())
+            .dest("visualise")
+            .help("Visualise the search");
+        parser.addArgument("-o", "--output")
+            .metavar("OUTPUT")
+            .dest("output")
+            .help("The output file to write the schedule to (default is INPUT-output.dot)");
 
-        final Test test = new Test("John", "20");
-        System.out.println(test.getName() + " is " + test.getAge() + " years old.");
+        try {
+            final Namespace namespace = parser.parseArgs(args);
+            System.out.println(namespace);
+        } catch (final ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+        }
     }
 }
