ecc9029ead44d7cb18e2a6c484ca0be302f02207
Henry Wang <hwan513@aucklanduni.ac.nz>
2023-09-25 23:20:29 +1300

    test: create test for models classes graph, node and edge

---
+++ src/test/java/nz/ac/auckland/se306/group12/GraphNodeEdgeModelTest.java
@@ -0,0 +1,129 @@
+package nz.ac.auckland.se306.group12;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import nz.ac.auckland.se306.group12.models.Edge;
+import nz.ac.auckland.se306.group12.models.Graph;
+import nz.ac.auckland.se306.group12.models.Node;
+
+
+/*
+ * Tests functionality of the model classes used for the graph classes
+ */
+class GraphNodeEdgeModelTest {
+
+  /**
+   * Tests node equality checks for the same name and weight
+   */
+  @Test
+  public void testNodeEquality() {
+    Node node1 = new Node("a", 1);
+    Node node2 = new Node("a", 1);
+    Node node3 = new Node("a", 2);
+
+    Assertions.assertEquals(node1, node2);
+    Assertions.assertNotEquals(node1, node3);
+    Assertions.assertNotEquals(node2, node3);
+  }
+
+  /**
+   * Test edge equality checks for the same source, destination and weight
+   */
+  @Test
+  public void testEdgeEquality() {
+    Node node1 = new Node("a", 1);
+    Node node2 = new Node("b", 1);
+    Node node3 = new Node("c", 1);
+    Edge edge1 = new Edge(node1, node2, 1);
+    Edge edge2 = new Edge(node1, node2, 1);
+    Edge edge3 = new Edge(node1, node2, 2);
+    Edge edge4 = new Edge(node1, node3, 1);
+
+    Assertions.assertEquals(edge1, edge2);
+    Assertions.assertNotEquals(edge1, edge3);
+    Assertions.assertNotEquals(edge2, edge3);
+    Assertions.assertNotEquals(edge1, edge4);
+  }
+
+  /**
+   * Test empty graphs are equal
+   */
+  @Test
+  public void testEmptyGraph() {
+    Graph graph1 = new Graph(new HashMap<>(), new HashSet<>());
+    Graph graph2 = new Graph(new HashMap<>(), new HashSet<>());
+
+    Assertions.assertEquals(graph1, graph2);
+  }
+
+  /**
+   * Test graph with same nodes is equal
+   */
+  @Test
+  public void testEqualGraphWithNode() {
+    Graph graph1 = new Graph(new HashMap<>(), new HashSet<>());
+    Graph graph2 = new Graph(new HashMap<>(), new HashSet<>());
+
+    graph1.getNodes().put("a", new Node("a", 1));
+    graph2.getNodes().put("a", new Node("a", 1));
+    Assertions.assertEquals(graph1, graph2);
+  }
+
+  /**
+   * Test graph with different node is not equal
+   */
+  @Test
+  public void testUnequalGraphWithNode() {
+    Graph graph1 = new Graph(new HashMap<>(), new HashSet<>());
+    Graph graph2 = new Graph(new HashMap<>(), new HashSet<>());
+
+    graph1.getNodes().put("a", new Node("a", 1));
+    graph2.getNodes().put("b", new Node("b", 1));
+
+    Assertions.assertNotEquals(graph1, graph2);
+  }
+
+  /**
+   * Test graph with same node and edge is equal
+   */
+  @Test
+  public void testEqualGraphWithNodesAndEdge() {
+    Graph graph1 = new Graph(new HashMap<>(), new HashSet<>());
+    Graph graph2 = new Graph(new HashMap<>(), new HashSet<>());
+
+    graph1.getNodes().put("a", new Node("a", 1));
+    graph2.getNodes().put("a", new Node("a", 1));
+
+    graph1.getNodes().put("b", new Node("b", 2));
+    graph2.getNodes().put("b", new Node("b", 2));
+
+    graph1.getEdges().add(new Edge(graph1.getNodes().get("a"), graph1.getNodes().get("b"), 1));
+    graph2.getEdges().add(new Edge(graph2.getNodes().get("a"), graph2.getNodes().get("b"), 1));
+
+    Assertions.assertEquals(graph1, graph2);
+  }
+
+  /**
+   * Test graph with same node and edge is not equal
+   */
+  @Test
+  public void testUnequalGraphWithNodesAndEdge() {
+    Graph graph1 = new Graph(new HashMap<>(), new HashSet<>());
+    Graph graph2 = new Graph(new HashMap<>(), new HashSet<>());
+
+    graph1.getNodes().put("a", new Node("a", 1));
+    graph2.getNodes().put("a", new Node("a", 1));
+
+    graph1.getNodes().put("b", new Node("b", 2));
+    graph2.getNodes().put("c", new Node("c", 2));
+
+    graph1.getEdges().add(new Edge(graph1.getNodes().get("a"), graph1.getNodes().get("b"), 1));
+    graph2.getEdges().add(new Edge(graph2.getNodes().get("a"), graph2.getNodes().get("b"), 1));
+
+    Assertions.assertNotEquals(graph1, graph2);
+  }
+
+}
