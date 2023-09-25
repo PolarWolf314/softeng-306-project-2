65ab3b762a6a1ca154289fd4d90834f7f6dc0d88
pumbas600 <joshjeffers600@gmail.com>
2023-09-25 14:34:22 +1300

    chore: :bricks: Reformat code using google style guide

--- src/main/java/nz/ac/auckland/se306/group12/Main.java
+++ src/main/java/nz/ac/auckland/se306/group12/Main.java
@@ -4,20 +4,17 @@ import nz.ac.auckland.se306.group12.cli.CommandLineParser;
 import nz.ac.auckland.se306.group12.models.CommandLineArguments;
 import nz.ac.auckland.se306.group12.parser.DotGraphIO;
 
-import java.io.File;
-
 public class Main {
     
-    public static void main(final String[] args) {
-        final CommandLineParser parser = new CommandLineParser();
-        final DotGraphIO dotGraphIO = new DotGraphIO();
+  public static void main(final String[] args) {
+    final CommandLineParser parser = new CommandLineParser();
+    final DotGraphIO dotGraphIO = new DotGraphIO();
 
-        final CommandLineArguments arguments = parser.parse(args);
-        dotGraphIO.readDotGraph(arguments.inputDotGraph());
-
+    final CommandLineArguments arguments = parser.parse(args);
+    dotGraphIO.readDotGraph(arguments.inputDotGraph());
 
 //
 //        System.out.println(arguments);
-    }
+  }
 
 }
--- src/main/java/nz/ac/auckland/se306/group12/parser/DotGraphIO.java
+++ src/main/java/nz/ac/auckland/se306/group12/parser/DotGraphIO.java
@@ -3,7 +3,6 @@ package nz.ac.auckland.se306.group12.parser;
 import com.paypal.digraph.parser.GraphEdge;
 import com.paypal.digraph.parser.GraphNode;
 import com.paypal.digraph.parser.GraphParser;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,29 +11,29 @@ import java.util.Map;
 // TODO: Use graph model classes when created
 public class DotGraphIO {
 
-    public Object readDotGraph(final File inputDotGraph) {
-        System.out.println(inputDotGraph.getPath());
-        try {
-            final GraphParser parser = new GraphParser(new FileInputStream(inputDotGraph));
+  public Object readDotGraph(final File inputDotGraph) {
+    System.out.println(inputDotGraph.getPath());
+    try {
+      final GraphParser parser = new GraphParser(new FileInputStream(inputDotGraph));
 
-            final Map<String, GraphNode> nodes = parser.getNodes();
-            final Map<String, GraphEdge> edges = parser.getEdges();
+      final Map<String, GraphNode> nodes = parser.getNodes();
+      final Map<String, GraphEdge> edges = parser.getEdges();
 
-            for (final GraphNode node : nodes.values()) {
-                System.out.println(node);
-            }
+      for (final GraphNode node : nodes.values()) {
+        System.out.println(node);
+      }
 
-            for (final GraphEdge edge : edges.values()) {
-                System.out.println(edge);
-            }
-        } catch (final IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+      for (final GraphEdge edge : edges.values()) {
+        System.out.println(edge);
+      }
+    } catch (final IOException e) {
+      e.printStackTrace();
     }
 
-    public void writeDotGraph(final File outputDotGraph, final Object dotGraph) {
+    return null;
+  }
 
-    }
+  public void writeDotGraph(final File outputDotGraph, final Object dotGraph) {
+
+  }
 }
