5948bffd79fc4fc15af201c31ee06fc627d8675e
Henry Wang <hwan513@aucklanduni.ac.nz>
2023-09-26 13:44:52 +1300

    style: remove unused import

--- src/main/java/nz/ac/auckland/se306/group12/models/Graph.java
+++ src/main/java/nz/ac/auckland/se306/group12/models/Graph.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
