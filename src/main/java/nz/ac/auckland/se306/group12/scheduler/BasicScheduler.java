eafff503d80d8056ec94e883007b2cd08d4103d2
PolarWolf314 <aaronjoshguo@gmail.com>
2023-09-27 14:08:49 +1300

    feat: created `BasicScheduler` class and method

---
+++ src/main/java/nz/ac/auckland/se306/group12/scheduler/BasicScheduler.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.se306.group12.scheduler;
+
+import java.util.List;
+import nz.ac.auckland.se306.group12.models.ScheduledTask;
+
+public class BasicScheduler {
+
+  public List<List<ScheduledTask>> getABasicSchedule(List<ScheduledTask> tasks) {
+    return null;
+  }
+
+}
