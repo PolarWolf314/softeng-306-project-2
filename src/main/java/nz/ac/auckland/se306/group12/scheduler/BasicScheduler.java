91da29980a769ca38603fc44bd6667d928c63a02
pumbas600 <joshjeffers600@gmail.com>
2023-09-30 17:21:38 +1300

    refactor: Try to make variable names more consistent

--- src/main/java/nz/ac/auckland/se306/group12/scheduler/BasicScheduler.java
+++ src/main/java/nz/ac/auckland/se306/group12/scheduler/BasicScheduler.java
@@ -57,28 +57,28 @@ public class BasicScheduler {
    * @param processors The list of processors to schedule on
    */
   private void scheduleTaskOnEarliestProcessor(Node task, List<Processor> processors) {
-    Set<Edge> parentEdges = task.getIncomingEdges();
+    Set<Edge> incomingEdges = task.getIncomingEdges();
 
     Processor earliestProcessor = processors.get(0);
     int earliestStartTime = Integer.MAX_VALUE;
 
-    if (parentEdges.isEmpty()) {
+    if (incomingEdges.isEmpty()) {
       earliestProcessor = this.getProcessorWithEarliestEndTime(processors);
       earliestStartTime = earliestProcessor.getEndTime();
     } else {
       IntercommunicationCosts communicationCosts = this.determineIntercommunicationCosts(
-          parentEdges, processors);
+          incomingEdges, processors);
 
       for (int processorIndex = 0; processorIndex < processors.size(); processorIndex++) {
         int processorEndTime = processors.get(processorIndex).getEndTime();
 
-        // If it's scheduled on the same processor as the latest parent, we don't need to consider
-        // the intercommunication cost, and so we use the second-latest parent end time. The
-        // processor end time will always be >= the latest parent on that processor.
-        int parentEndTime = processorIndex == communicationCosts.latestProcessorIndex()
+        // If it's scheduled on the same processor as the latest dependency, we don't need to consider
+        // the intercommunication cost, and so we use the second-latest start time. The
+        // processor end time will always be >= the latest dependency on that processor.
+        int dependentStartTime = processorIndex == communicationCosts.latestProcessorIndex()
             ? communicationCosts.secondLatestStartTime() : communicationCosts.latestStartTime();
 
-        int earliestProcessorStartTime = Math.max(processorEndTime, parentEndTime);
+        int earliestProcessorStartTime = Math.max(processorEndTime, dependentStartTime);
         if (earliestProcessorStartTime < earliestStartTime) {
           earliestStartTime = earliestProcessorStartTime;
           earliestProcessor = processors.get(processorIndex);
@@ -91,41 +91,41 @@ public class BasicScheduler {
   }
 
   /**
-   * Determines the intercommunication costs for the given parent edges and processors. We only need
-   * to find the latest and second-latest start times while factoring in the communication cost as
-   * if the task is scheduled on the same processor as the latest start time we switch to the
+   * Determines the intercommunication costs for the given incoming edges and processors. We only
+   * need to find the latest and second-latest start times while factoring in the communication cost
+   * as if the task is scheduled on the same processor as the latest start time we switch to the
    * second-latest start time (As the intercommunication cost doesn't apply when scheduled on the
    * same processor). In all other cases, we use the latest start time.
    *
-   * @param parentEdges The edges connecting the dependencies of the task
-   * @param processors  The list of processors that can be scheduled on
+   * @param incomingEdges The edges connecting the dependencies of the task
+   * @param processors    The list of processors that can be scheduled on
    * @return The intercommunication-factored latest and second-latest start times
    */
   public IntercommunicationCosts determineIntercommunicationCosts(
-      Set<Edge> parentEdges,
+      Set<Edge> incomingEdges,
       List<Processor> processors
   ) {
     int secondLatestStartTime = Integer.MIN_VALUE;
-    Node latestParent = null;
+    Node latestDependency = null;
     int latestStartTime = Integer.MIN_VALUE;
 
-    // Determine the latest and second-latest parents assuming the task is scheduled on a different
-    // processor and therefore, the communication cost must be considered. The latest parent node is
+    // Determine the latest and second-latest dependencies assuming the task is scheduled on a different
+    // processor and therefore, the communication cost must be considered. The latest dependency is
     // also stored so that we can determine which processor it is scheduled on.
-    for (Edge parentEdge : parentEdges) {
-      final Node parent = parentEdge.getSource();
-      int newStartTime = parent.getEndTime() + parentEdge.getWeight();
+    for (Edge incomingEdge : incomingEdges) {
+      final Node dependency = incomingEdge.getSource();
+      int dependentStartTime = dependency.getEndTime() + incomingEdge.getWeight();
 
-      if (newStartTime > latestStartTime) {
+      if (dependentStartTime > latestStartTime) {
         secondLatestStartTime = latestStartTime;
-        latestParent = parent;
-        latestStartTime = newStartTime;
-      } else if (newStartTime > secondLatestStartTime) {
-        secondLatestStartTime = newStartTime;
+        latestDependency = dependency;
+        latestStartTime = dependentStartTime;
+      } else if (dependentStartTime > secondLatestStartTime) {
+        secondLatestStartTime = dependentStartTime;
       }
     }
 
-    int latestProcessorIndex = this.getParentProcessor(latestParent, processors)
+    int latestProcessorIndex = this.getParentProcessor(latestDependency, processors)
         .getProcessorIndex();
 
     return new IntercommunicationCosts(
