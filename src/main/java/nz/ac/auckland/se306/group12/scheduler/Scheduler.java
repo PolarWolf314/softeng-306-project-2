fee90440c95e995588581e46f736cbe8cbd85337
pumbas600 <joshjeffers600@gmail.com>
2023-10-02 00:07:12 +1300

    chore: Minor sylistic changes

--- src/main/java/nz/ac/auckland/se306/group12/scheduler/Scheduler.java
+++ src/main/java/nz/ac/auckland/se306/group12/scheduler/Scheduler.java
@@ -12,9 +12,10 @@ public interface Scheduler {
    * dependences and that, if scheduled on a different processor, after the end time + transfer time
    * of its dependence. No two tasks can be scheduled on the same processor at the same time.
    *
-   * @param graph          The graph representing the tasks to be scheduled
+   * @param graph          The {@link Graph} representing the tasks to be scheduled
    * @param processorCount The number of processors to schedule the tasks on
    * @return A valid schedule for the given graph of tasks on the processors
    */
   List<Processor> schedule(Graph graph, int processorCount);
+
 }
