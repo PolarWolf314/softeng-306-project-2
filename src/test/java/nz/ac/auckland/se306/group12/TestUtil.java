002d508f1724c34ee04624da3989f890570753f6
pumbas600 <joshjeffers600@gmail.com>
2023-10-04 13:38:33 +1300

    fix: Return placeholder scheduler when getting list of optimal schedulers

--- src/test/java/nz/ac/auckland/se306/group12/TestUtil.java
+++ src/test/java/nz/ac/auckland/se306/group12/TestUtil.java
@@ -2,10 +2,10 @@ package nz.ac.auckland.se306.group12;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 import nz.ac.auckland.se306.group12.io.DotGraphIO;
 import nz.ac.auckland.se306.group12.models.Graph;
+import nz.ac.auckland.se306.group12.models.Schedule;
 import nz.ac.auckland.se306.group12.scheduler.Scheduler;
 import org.junit.jupiter.api.Assertions;
 
@@ -35,7 +35,15 @@ public class TestUtil {
    * @return The schedulers to test
    */
   public static List<Scheduler> getOptimalSchedulers() {
-    return Collections.emptyList();
+    // TODO: Return instances of the optimal schedulers
+    Scheduler placeholderScheduler = new Scheduler() {
+      @Override
+      public Schedule schedule(Graph graph, int processorCount) {
+        return null;
+      }
+    };
+
+    return List.of(placeholderScheduler);
   }
 
 }
