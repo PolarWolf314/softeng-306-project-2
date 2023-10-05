246fb9c0feb73485c96c1be7cce00e1071e3e2c8
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-05 18:49:02 +1300

    feat: new runtime exceptions

---
+++ src/main/java/nz/ac/auckland/se306/group12/exceptions/DanglingEdgeException.java
@@ -0,0 +1,8 @@
+package nz.ac.auckland.se306.group12.exceptions;
+
+public class DanglingEdgeException extends RuntimeException {
+
+  public DanglingEdgeException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/nz/ac/auckland/se306/group12/exceptions/IllegalEdgeWeightException.java
@@ -0,0 +1,9 @@
+package nz.ac.auckland.se306.group12.exceptions;
+
+public class IllegalEdgeWeightException extends RuntimeException {
+
+  public IllegalEdgeWeightException(String message) {
+    super(message);
+  }
+
+}
