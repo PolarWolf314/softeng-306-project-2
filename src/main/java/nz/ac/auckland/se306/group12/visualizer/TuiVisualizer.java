0b1c45940f514eeeb8058ab2ea7ac171e171c71b
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-07 16:47:41 +1300

    feat: vertical Gantt chart

--- src/main/java/nz/ac/auckland/se306/group12/visualizer/TuiVisualizer.java
+++ src/main/java/nz/ac/auckland/se306/group12/visualizer/TuiVisualizer.java
@@ -1,7 +1,9 @@
 package nz.ac.auckland.se306.group12.visualizer;
 
+import java.util.Arrays;
 import nz.ac.auckland.se306.group12.models.Graph;
 import nz.ac.auckland.se306.group12.models.Schedule;
+import nz.ac.auckland.se306.group12.models.ScheduledTask;
 
 public class TuiVisualizer implements Visualizer {
 
@@ -24,15 +26,42 @@ public class TuiVisualizer implements Visualizer {
     this.populateStatusBar();
     sb.append(NEW_LINE);
 
-    // Gantt Chart Header
+    this.updateGanttChart(schedule);
+    sb.append(NEW_LINE);
+
+    System.out.println(sb);
+  }
+
+  private void updateGanttChart(Schedule schedule) {
+    // Chart header
     for (int processorIndex = 1; processorIndex <= schedule.getProcessorEndTimes().length;
         processorIndex++) {
       sb.append(String.format("P%-7s", processorIndex));
     }
-
     sb.append(NEW_LINE);
 
-    System.out.println(sb);
+    // Chart body
+    int[][] verticalGantt = this.scheduleToVerticalGantt(schedule);
+    for (int[] unitTime : verticalGantt) {
+      for (int activeTaskIndex : unitTime) {
+
+        if (activeTaskIndex == -1) {
+          // Processor idling
+          sb.append(new AnsiEscapeSequenceBuilder().background(7))
+              .append("       ");  // 7 spaces (chart columns are 7ch long, excl. padding)
+        } else {
+          // Background colours constrained to SRG codes 196 through 213
+          sb.append(new AnsiEscapeSequenceBuilder().foreground(255, 255, 255)
+                  .background(activeTaskIndex % 18 + 196))
+              .append(String.format(" %-5d ", activeTaskIndex));
+        }
+
+        sb.append(new AnsiEscapeSequenceBuilder().reset()).append(" "); // Padding between columns
+
+      }
+
+      sb.append(NEW_LINE);
+    }
   }
 
   private void populateStatusBar() {
@@ -48,8 +77,38 @@ public class TuiVisualizer implements Visualizer {
         .append(NEW_LINE);
   }
 
-  private char[][] flatten(Schedule schedule) {
-    return null;
+  private int[][] scheduleToVerticalGantt(Schedule schedule) {
+    return scheduleToGantt(schedule, true);
+  }
+
+  private int[][] scheduleToGantt(Schedule schedule, boolean transpose) {
+    final int IDLE = -1;
+
+    int processorCount = schedule.getProcessorEndTimes().length;
+    int makespan = schedule.getLatestEndTime();
+
+    // Initialise Gantt matrix where all cores idle for the entire makespan
+    int[][] scheduleMatrix = transpose
+        ? new int[makespan][processorCount]
+        : new int[processorCount][makespan];
+    for (int[] row : scheduleMatrix) {
+      Arrays.fill(row, IDLE);
+    }
+
+    ScheduledTask[] tasks = schedule.getScheduledTasks();
+    for (int i = 0, taskCount = schedule.getScheduledTaskCount(); i < taskCount; i++) {
+      ScheduledTask task = tasks[i];
+      int processorIndex = task.getProcessorIndex();
+      for (int time = task.getStartTime(); time < task.getEndTime(); time++) {
+        if (transpose) {
+          scheduleMatrix[time][processorIndex] = i;
+        } else {
+          scheduleMatrix[processorIndex][time] = i;
+        }
+      }
+    }
+
+    return scheduleMatrix;
   }
 
 }
