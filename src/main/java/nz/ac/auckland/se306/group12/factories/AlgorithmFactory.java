6e6c1126081529fd9e2ed6a87b8bf08642b4907f
PolarWolf314 <aaronjoshguo@gmail.com>
2023-10-08 20:26:46 +1300

    feat: added method to `getScheduler`

--- src/main/java/nz/ac/auckland/se306/group12/factories/AlgorithmFactory.java
+++ src/main/java/nz/ac/auckland/se306/group12/factories/AlgorithmFactory.java
@@ -1,5 +1,24 @@
 package nz.ac.auckland.se306.group12.factories;
 
+import nz.ac.auckland.se306.group12.scheduler.AStarScheduler;
+import nz.ac.auckland.se306.group12.scheduler.DfsScheduler;
+import nz.ac.auckland.se306.group12.scheduler.Scheduler;
+
 public class AlgorithmFactory {
 
+  /**
+   * Returns a scheduler for the given algorithm
+   *
+   * @param algorithm The algorithm to return a scheduler for
+   * @return A scheduler for the given algorithm
+   */
+  public Scheduler getScheduler(String algorithm) {
+    if (algorithm.equals("dfs")) {
+      return new DfsScheduler();
+    } else if (algorithm.equals("astar")) {
+      return new AStarScheduler();
+    } else {
+      throw new IllegalArgumentException("Invalid algorithm");
+    }
+  }
 }
