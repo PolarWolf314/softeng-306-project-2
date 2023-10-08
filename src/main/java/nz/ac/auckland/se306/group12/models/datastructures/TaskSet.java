a252c708fc62cb5675c10298e50d5d5c6295253c
pumbas600 <joshjeffers600@gmail.com>
2023-10-08 16:06:06 +1300

    feat: Make `TaskSet` implement the `Set` interface

--- src/main/java/nz/ac/auckland/se306/group12/models/datastructures/TaskSet.java
+++ src/main/java/nz/ac/auckland/se306/group12/models/datastructures/TaskSet.java
@@ -1,12 +1,14 @@
 package nz.ac.auckland.se306.group12.models.datastructures;
 
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import nz.ac.auckland.se306.group12.models.Graph;
 import nz.ac.auckland.se306.group12.models.Task;
 
 @RequiredArgsConstructor
-public class TaskSet implements Iterable<Task> {
+public class TaskSet implements Set<Task> {
 
   /**
    * As we are using an int to store the bitmap, we can only store up to 32 tasks, or a maximum
@@ -18,29 +20,46 @@ public class TaskSet implements Iterable<Task> {
   private int taskBitMap = 0;
   private int taskCount = 0;
 
+  public TaskSet(Set<Task> existingTaskSet) {
+    if (!(existingTaskSet instanceof TaskSet taskSet)) {
+      throw new IllegalArgumentException("TaskSet can only be constructed from another TaskSet");
+    }
+
+    this.taskBitMap = taskSet.taskBitMap;
+    this.taskCount = taskSet.taskCount;
+    this.taskGraph = taskSet.taskGraph;
+  }
+
   public TaskSet(TaskSet existingTaskSet) {
     this.taskBitMap = existingTaskSet.taskBitMap;
     this.taskCount = existingTaskSet.taskCount;
     this.taskGraph = existingTaskSet.taskGraph;
   }
 
-
+  @Override
   public int size() {
     return this.taskCount;
   }
 
+  @Override
   public boolean isEmpty() {
     return this.taskCount == 0;
   }
 
-  public boolean contains(Task task) {
-    return this.containsTaskIndex(task.getIndex());
+  @Override
+  public boolean contains(Object object) {
+    if (object instanceof Task task) {
+      return this.containsTaskIndex(task.getIndex());
+    }
+    return false;
   }
 
   public boolean containsTaskIndex(int taskIndex) {
+    // Check that there is a 1 bit at the taskIndex
     return (this.taskBitMap & (1 << taskIndex)) != 0;
   }
 
+  @Override
   public boolean add(Task task) {
     boolean isContained = this.contains(task);
     if (isContained) {
@@ -53,21 +72,57 @@ public class TaskSet implements Iterable<Task> {
               MAX_TASK_INDEX));
     }
 
+    // Add a 1 bit to the taskBitMap at the index of the task
     this.taskBitMap |= (1 << task.getIndex());
     this.taskCount++;
     return true;
   }
 
-  public boolean remove(Task task) {
-    if (!this.contains(task)) {
+  @Override
+  public boolean remove(Object object) {
+    if (!this.contains(object)) {
       return false;
     }
 
+    // We know that the object is a task because of the contains check
+    Task task = (Task) object;
     this.taskBitMap &= ~(1 << task.getIndex());
     this.taskCount--;
     return true;
   }
 
+  @Override
+  public boolean containsAll(Collection<?> collection) {
+    for (Object object : collection) {
+      if (!this.contains(object)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  @Override
+  public boolean addAll(Collection<? extends Task> collection) {
+    int oldTaskCount = this.taskCount;
+    for (Task task : collection) {
+      this.add(task);
+    }
+
+    return this.taskCount != oldTaskCount;
+  }
+
+  @Override
+  public boolean retainAll(Collection<?> collection) {
+    return this.removeIf(task -> !collection.contains(task));
+  }
+
+  @Override
+  public boolean removeAll(Collection<?> collection) {
+    return this.removeIf(collection::contains);
+  }
+
+  @Override
   public void clear() {
     this.taskCount = 0;
     this.taskBitMap = 0;
@@ -99,4 +154,15 @@ public class TaskSet implements Iterable<Task> {
       }
     };
   }
+
+  @Override
+  public Object[] toArray() {
+    throw new UnsupportedOperationException("toArray() is not supported on TaskSet");
+  }
+
+  @Override
+  public <T> T[] toArray(T[] array) {
+    throw new UnsupportedOperationException("toArray() is not supported on TaskSet");
+  }
+
 }
