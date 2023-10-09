315834d2842a3b62ea75e50407240b2738928852
pumbas600 <joshjeffers600@gmail.com>
2023-10-09 18:30:50 +1300

    feat: Add an indexable and indexable resolver interface

---
+++ src/main/java/nz/ac/auckland/se306/group12/models/datastructures/Indexable.java
@@ -0,0 +1,10 @@
+package nz.ac.auckland.se306.group12.models.datastructures;
+
+public interface Indexable {
+
+  /**
+   * @return The index corresponding to this object.
+   */
+  int getIndex();
+  
+}
