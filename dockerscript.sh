48301d76086155b5978894d4b802ee30abfbc2c5
PolarWolf314 <aaronjoshguo@gmail.com>
2023-10-10 23:23:15 +1300

    fix: fixed script and dockerfile

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,14 +1,14 @@
-# kills all running docker containers
-docker kill "$(docker ps -q)"
+# stops all running docker containers
+docker stop $(docker ps -aq)
 
 # removes all docker containers
-docker rm "$(docker ps -a -q)"
+docker rm $(docker ps -aq)
 
 # deletes all docker images
-docker rmi "$(docker image -q)"
+docker rmi "$(docker images -q)"
 
 # creates a new docker image
 docker build -t se306 .
 
 # runs the new docker image in a container with 50% cpu utilisation
-docker run --cpus 6 se306
+docker run --cpus 6 -v $(pwd)/build/test-results:/app/build/test-results se306
