a9b47ecb307195bb30aaafd7b2ffe92c6ac8acc5
pumbas600 <joshjeffers600@gmail.com>
2023-10-10 21:38:49 +1300

    feat: Implement A*

--- src/main/java/nz/ac/auckland/se306/group12/scheduler/AStarScheduler.java
+++ src/main/java/nz/ac/auckland/se306/group12/scheduler/AStarScheduler.java
@@ -1,23 +1,66 @@
 package nz.ac.auckland.se306.group12.scheduler;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import lombok.Getter;
 import nz.ac.auckland.se306.group12.models.Graph;
 import nz.ac.auckland.se306.group12.models.Schedule;
+import nz.ac.auckland.se306.group12.models.ScheduledTask;
 import nz.ac.auckland.se306.group12.models.SchedulerStatus;
+import nz.ac.auckland.se306.group12.models.Task;
 
 @Getter
 public class AStarScheduler implements Scheduler {
 
   private long searchedCount;
   private long prunedCount;
-  private Schedule bestSchedule;
   private SchedulerStatus status = SchedulerStatus.IDLE;
+  private Queue<Schedule> priorityQueue = new PriorityQueue<>();
 
   @Override
-  public Schedule schedule(Graph graph, int processorCount) {
-    // TODO Auto-generated method stub
-    System.out.println("A* Scheduler not implemented");
-    return null;
+  public Schedule getBestSchedule() {
+    return this.priorityQueue.peek();
+  }
+
+  @Override
+  public Schedule schedule(Graph taskGraph, int processorCount) {
+    this.priorityQueue.clear();
+    this.status = SchedulerStatus.SCHEDULING;
+    Deque<Schedule> stack = new ArrayDeque<>();
+
+    stack.push(new Schedule(taskGraph, processorCount));
+
+    // DFS iteration (no optimisations)
+    while (!stack.isEmpty()) {
+      Schedule currentSchedule = stack.peek();
+      this.searchedCount++;
+
+      // Check if current schedule is complete. The first complete schedule is the best schedule
+      if (currentSchedule.getScheduledTaskCount() == taskGraph.taskCount()) {
+        this.status = SchedulerStatus.SCHEDULED;
+        return currentSchedule;
+      }
+
+      // Only remove it now so that if we found the best schedule we can still access it through peeking
+      stack.pop();
+
+      // Check to find if any tasks can be scheduled and schedule them
+      for (Task task : currentSchedule.getReadyTasks()) {
+        int[] latestStartTimes = currentSchedule.getLatestStartTimesOf(task);
+        for (int i = 0; i < latestStartTimes.length; i++) {
+          // Ensure that it either schedules by latest time or after the last task on the processor
+          int startTime = Math.max(latestStartTimes[i], currentSchedule.getProcessorEndTimes()[i]);
+          int endTime = startTime + task.getWeight();
+          ScheduledTask newScheduledTask = new ScheduledTask(startTime, endTime, i);
+          stack.push(currentSchedule.extendWithTask(newScheduledTask, task));
+        }
+      }
+    }
+
+    this.status = SchedulerStatus.SCHEDULED;
+    throw new IllegalStateException("No optimal schedule found");
   }
 
 }
