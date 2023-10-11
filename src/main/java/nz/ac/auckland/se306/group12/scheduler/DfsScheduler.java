06584c320151070a35ae4edf9388f6595cfff571
Tinchuu <65702538+Tinchuu@users.noreply.github.com>
2023-10-11 13:59:22 +1300

    refactor: did some refactoring to DfsScheduler to allow for multithreading

--- src/main/java/nz/ac/auckland/se306/group12/scheduler/DfsScheduler.java
+++ src/main/java/nz/ac/auckland/se306/group12/scheduler/DfsScheduler.java
@@ -2,8 +2,11 @@ package nz.ac.auckland.se306.group12.scheduler;
 
 import java.util.ArrayDeque;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Queue;
-
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import lombok.Getter;
 import nz.ac.auckland.se306.group12.models.Graph;
 import nz.ac.auckland.se306.group12.models.Schedule;
@@ -11,16 +14,26 @@ import nz.ac.auckland.se306.group12.models.SchedulerStatus;
 
 public class DfsScheduler implements Scheduler {
 
-  private int currentMinMakespan = Integer.MAX_VALUE;
+  private AtomicInteger currentMinMakespan = new AtomicInteger(Integer.MAX_VALUE);
 
-  @Getter
-  private long searchedCount = 0;
-  @Getter
-  private long prunedCount = 0;
+  private AtomicLong searchedCount = new AtomicLong(0);
+  private AtomicLong prunedCount = new AtomicLong(0);
   @Getter
   private SchedulerStatus status = SchedulerStatus.IDLE;
   @Getter
   private Schedule bestSchedule = null;
+
+
+  @Override
+  public long getSearchedCount() {
+    return this.searchedCount.getOpaque();
+  }
+
+  @Override
+  public long getPrunedCount() {
+    return this.prunedCount.getOpaque();
+  }
+
 
   /*
    * @inheritDoc
@@ -28,25 +41,34 @@ public class DfsScheduler implements Scheduler {
   @Override
   public Schedule schedule(Graph taskGraph, int processorCount) {
     this.status = SchedulerStatus.SCHEDULING;
+
+    Set<Thread> threads = new HashSet<>();
     Queue<Schedule> queue = Collections.asLifoQueue(new ArrayDeque<>());
 
     queue.add(new Schedule(taskGraph, processorCount));
 
+    branchAndBound(taskGraph, queue);
+
+    this.status = SchedulerStatus.SCHEDULED;
+    return this.bestSchedule;
+  }
+
+  private void branchAndBound(Graph taskGraph, Queue<Schedule> queue) {
     // DFS iteration (no optimisations)
     while (!queue.isEmpty()) {
       Schedule currentSchedule = queue.remove();
 
       // Prune if current schedule is worse than current best
-      if (currentSchedule.getEstimatedMakespan() >= this.currentMinMakespan) {
-        this.prunedCount++;
+      if (currentSchedule.getEstimatedMakespan() >= this.currentMinMakespan.get()) {
+        this.prunedCount.incrementAndGet();
         continue;
       }
 
-      this.searchedCount++;
+      this.searchedCount.incrementAndGet();
 
       // Check if current schedule is complete
       if (currentSchedule.getScheduledTaskCount() == taskGraph.taskCount()) {
-        this.currentMinMakespan = currentSchedule.getLatestEndTime();
+        this.currentMinMakespan = new AtomicInteger(currentSchedule.getLatestEndTime());
         this.bestSchedule = currentSchedule;
         continue;
       }
@@ -54,8 +76,6 @@ public class DfsScheduler implements Scheduler {
       currentSchedule.extendSchedule(queue);
     }
 
-    this.status = SchedulerStatus.SCHEDULED;
-    return this.bestSchedule;
   }
 
 }
