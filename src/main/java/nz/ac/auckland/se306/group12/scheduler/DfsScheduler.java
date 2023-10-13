fc7cc724018b8324333a8ea4e5739c3e2f4f914b
Tinchuu <65702538+Tinchuu@users.noreply.github.com>
2023-10-14 12:07:44 +1300

    feat: non atomic metrics

--- src/main/java/nz/ac/auckland/se306/group12/scheduler/DfsScheduler.java
+++ src/main/java/nz/ac/auckland/se306/group12/scheduler/DfsScheduler.java
@@ -2,11 +2,10 @@ package nz.ac.auckland.se306.group12.scheduler;
 
 import java.util.ArrayDeque;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 import lombok.Getter;
 import nz.ac.auckland.se306.group12.models.Graph;
@@ -17,26 +16,19 @@ import nz.ac.auckland.se306.group12.models.Task;
 
 public class DfsScheduler implements Scheduler {
 
-  private final AtomicLong searchedCount = new AtomicLong(0);
-  private final AtomicLong prunedCount = new AtomicLong(0);
   private final AtomicReference<Schedule> bestSchedule = new AtomicReference<>();
+  @Getter
+  private long searchedCount = 0;
+  @Getter
+  private long prunedCount = 0;
+  private long[] searchCountArray;
+  private long[] prunedCountArray;
   private AtomicInteger currentMinMakespan = new AtomicInteger(Integer.MAX_VALUE);
   @Getter
   private SchedulerStatus status = SchedulerStatus.IDLE;
-  private Set<Thread> threads = ConcurrentHashMap.newKeySet();
+  private Map<Thread, Integer> threads = new ConcurrentHashMap<>();
   private AtomicInteger threadCount = new AtomicInteger(1);
   private int threadNum = 1;
-
-
-  @Override
-  public long getSearchedCount() {
-    return this.searchedCount.get();
-  }
-
-  @Override
-  public long getPrunedCount() {
-    return this.prunedCount.get();
-  }
 
   @Override
   public Schedule getBestSchedule() {
@@ -51,13 +43,15 @@ public class DfsScheduler implements Scheduler {
   public Schedule schedule(Graph taskGraph, int processorCount) {
     this.status = SchedulerStatus.SCHEDULING;
     this.threadNum = 4;
+    this.searchCountArray = new long[this.threadNum];
+    this.prunedCountArray = new long[this.threadNum];
 
     Queue<Schedule> queue = Collections.asLifoQueue(new ArrayDeque<>());
     queue.add(new Schedule(taskGraph, processorCount));
 
-    branchAndBound(taskGraph, queue);
+    branchAndBound(taskGraph, queue, 1);
 
-    for (Thread thread : this.threads) {
+    for (Thread thread : this.threads.keySet()) {
       try {
         thread.join();
       } catch (InterruptedException e) {
@@ -65,22 +59,33 @@ public class DfsScheduler implements Scheduler {
       }
     }
 
+    for (int i = 0; i < this.threadNum; i++) {
+      this.prunedCount += this.prunedCountArray[i];
+      this.searchedCount += searchCountArray[i];
+    }
+
     this.status = SchedulerStatus.SCHEDULED;
     return this.bestSchedule.get();
   }
 
-  private void branchAndBound(Graph taskGraph, Queue<Schedule> queue) {
+  /**
+   * Performs branch and bound algorithm on a given graph.
+   *
+   * @param taskGraph graph to perform branch and bound on
+   * @param queue     current queue of the branch and bound instance
+   */
+  private void branchAndBound(Graph taskGraph, Queue<Schedule> queue, int threadIndex) {
     // DFS iteration (no optimisations)
     while (!queue.isEmpty()) {
       Schedule currentSchedule = queue.remove();
 
       // Prune if current schedule is worse than current best
       if (currentSchedule.getEstimatedMakespan() >= this.currentMinMakespan.get()) {
-        this.prunedCount.incrementAndGet();
+        this.prunedCountArray[threadIndex]++;
         continue;
       }
 
-      this.searchedCount.incrementAndGet();
+      this.searchCountArray[threadIndex]++;
 
       // Check if current schedule is complete
       if (currentSchedule.getScheduledTaskCount() == taskGraph.taskCount()) {
@@ -97,15 +102,17 @@ public class DfsScheduler implements Scheduler {
           int endTime = startTime + task.getWeight();
           ScheduledTask newScheduledTask = new ScheduledTask(startTime, endTime, i);
 
-          if (this.threadCount.getAndIncrement() < this.threadNum) {
+          int currentThread = this.threadCount.getAndIncrement();
+          if (currentThread < this.threadNum) {
             Thread thread = new Thread(() -> {
               Queue<Schedule> currentQueue = Collections.asLifoQueue(new ArrayDeque<>());
               currentQueue.add(currentSchedule.extendWithTask(newScheduledTask, task));
-              branchAndBound(taskGraph, currentQueue);
+              branchAndBound(taskGraph, currentQueue, currentThread);
             });
-            this.threads.add(thread);
+            this.threads.put(thread, currentThread);
             thread.start();
           } else {
+            this.threadCount.decrementAndGet();
             queue.add(currentSchedule.extendWithTask(newScheduledTask, task));
           }
 
