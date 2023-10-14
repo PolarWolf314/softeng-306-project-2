eb6aeb1dc8dd734cdf53cf88666536fc98b85d9b
Henry Wang <hwan513@aucklanduni.ac.nz>
2023-10-15 12:52:22 +1300

    refactor: remove unused print allocation method

--- src/main/java/nz/ac/auckland/se306/group12/scheduler/DfsAOScheduler.java
+++ src/main/java/nz/ac/auckland/se306/group12/scheduler/DfsAOScheduler.java
@@ -50,9 +50,7 @@ public class DfsAOScheduler implements Scheduler {
 
       // Check if current allocation is complete
       if (currentAllocation.getAllocationCount() == taskGraph.taskCount()) {
-        // TODO: do the ordering algorithm
         order(currentAllocation, taskGraph);
-        // printAllProcessors(currentAllocation);
         continue;
       }
       currentAllocation.extendAllocation(stack);
@@ -88,26 +86,6 @@ public class DfsAOScheduler implements Scheduler {
       currentSchedule.extendSchedule(queue);
     }
 
-  }
-
-  private void printAllProcessors(Allocation currentAllocation) {
-    System.out.println("Allocation");
-    for (int i = 0; i < currentAllocation.getProcessors().length; i++) {
-      Set<Task> processor = currentAllocation.getProcessors()[i];
-      if (processor == null) {
-        continue;
-      }
-      System.out.print("Processor " + i + ": ");
-      for (Task task : processor) {
-        System.out.print(task.getLabel() + ", ");
-      }
-      System.out.println();
-    }
-    for (int i = 0; i < currentAllocation.getProcessors().length; i++) {
-      System.out.print(currentAllocation.getProcessorWeights()[i] + ", ");
-    }
-    System.out.println();
-    System.out.println();
   }
 
 }
