434110a3a149e8f732303c502df6967dfbe502cb
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-16 02:13:46 +1300

    style: spacing

--- src/main/java/nz/ac/auckland/se306/group12/cli/CommandLineParser.java
+++ src/main/java/nz/ac/auckland/se306/group12/cli/CommandLineParser.java
@@ -37,8 +37,8 @@ public class CommandLineParser {
             + System.getProperty("line.separator").repeat(2)
             + "The visualiser renders in the terminal from which you initiate this program, and "
             + "will expand to fill the space it is given.  Using a relatively large window size is "
-            + "recommended (at least 100 x 40), though you should probably just maximise the "
-            + "window.  (No, really, you should.)"
+            + "recommended (at least 100x40), though you should probably just maximise the window. "
+            + " (No, really, you should.)"
         );
     this.parser.addArgument(Keys.INPUT_DOT_GRAPH)
         .metavar("INPUT.dot")
