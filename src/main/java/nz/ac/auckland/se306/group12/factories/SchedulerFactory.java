b4d4a9e25b908dc00d21f925c9eabc4d769e83dd
pumbas600 <joshjeffers600@gmail.com>
2023-10-16 03:25:42 +1300

    feat: Update factory to use parallel dfs ao

--- src/main/java/nz/ac/auckland/se306/group12/factories/SchedulerFactory.java
+++ src/main/java/nz/ac/auckland/se306/group12/factories/SchedulerFactory.java
@@ -4,32 +4,10 @@ import nz.ac.auckland.se306.group12.models.CommandLineArguments;
 import nz.ac.auckland.se306.group12.scheduler.AStarScheduler;
 import nz.ac.auckland.se306.group12.scheduler.DfsAOScheduler;
 import nz.ac.auckland.se306.group12.scheduler.DfsScheduler;
+import nz.ac.auckland.se306.group12.scheduler.ParallelDfsAOScheduler;
 import nz.ac.auckland.se306.group12.scheduler.Scheduler;
 
 public class SchedulerFactory {
-
-  /**
-   * Returns a scheduler for the given algorithm
-   *
-   * @param algorithm The command line arguments that this was run with
-   * @return A scheduler for the given algorithm
-   */
-  public Scheduler getScheduler(String algorithm) {
-
-    switch (algorithm.toLowerCase()) {
-      case "astar" -> {
-        return new AStarScheduler();
-      }
-      case "dfs" -> {
-        return new DfsScheduler();
-      }
-      case "ao" -> {
-        return new DfsAOScheduler();
-      }
-      default -> throw new IllegalArgumentException(
-          "Invalid algorithm. " + algorithm + " is not a valid algorithm.");
-    }
-  }
 
   /**
    * Returns a scheduler depending on if the user wants to visualise the search or not since we do
@@ -39,11 +17,27 @@ public class SchedulerFactory {
    * @return A scheduler depending on if the user wants to visualise the search or not
    */
   public Scheduler getScheduler(CommandLineArguments arguments) {
-    // We only support parallelization and visualization using DFS
-    if (arguments.visualiseSearch() || arguments.parallelisationProcessorCount() > 1) {
+    // We only support visualization using DFS
+    if (arguments.visualiseSearch()) {
       return new DfsScheduler(arguments.parallelisationProcessorCount());
-    } else {
-      return this.getScheduler(arguments.algorithm());
     }
+
+    switch (arguments.algorithm().toLowerCase()) {
+      case "astar" -> {
+        return new AStarScheduler();
+      }
+      case "dfs" -> {
+        return new DfsScheduler(arguments.parallelisationProcessorCount());
+      }
+      case "ao" -> {
+        return arguments.parallelisationProcessorCount() > 1
+            ? new ParallelDfsAOScheduler(arguments.parallelisationProcessorCount())
+            : new DfsAOScheduler();
+      }
+      default -> throw new IllegalArgumentException(
+          "Invalid algorithm. " + arguments.algorithm() + " is not a valid algorithm.");
+    }
+
   }
+
 }
